750ceaea85caa2621eae1b6879beb8f0268d2278
chiepu3 <84263077+chiepu3@users.noreply.github.com>
2023-11-08 15:09:38 +0900

    feat: 貸出のテーブルを作ろうとしたがやる気が持たなかった

--- src/reception/pages/borrow_comp/Borrow_complete.tsx
+++ src/reception/pages/borrow_comp/Borrow_complete.tsx
@@ -1,6 +1,17 @@
 
 import './done.css'
 import './borrow_complete.css'
+import { Card, CardContent, Typography, createTheme } from '@mui/material';
+
+
+const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+  
+
+
 export default function Borrow_complete() {
     return (
         <>
@@ -18,7 +29,21 @@ export default function Borrow_complete() {
             <div className="text-container">
                 手続きが完了しました！
             </div>
+            <br></br>
+            <Card sx={{maxWidth:300,margin:"auto",padding:"10px",background: "#f8f8f8"}}>
+                <CardContent>
+                    <Typography variant="h5" component="div">
+                        受付番号
+                    </Typography>
+                    <Typography variant="h4">
+                        3218
+                    </Typography>
+                    <Typography>
+                        この受付番号を控えてください。
+                    </Typography>
+                </CardContent>
 
+            </Card>
         </>
     );
 }
