f04fa7d4f17493b349b36369ce66b044e4c435ca
sobonn <67411620+sobonn@users.noreply.github.com>
2023-12-10 23:38:40 +0900

    feat: berry-free-react-admin ダッシュボードの追加
    `/dashboard`以下にダッシュボードのテンプレートを貼り付けた

---
+++ src/dashboard/App.jsx
@@ -0,0 +1,34 @@
+import { useSelector } from 'react-redux';
+
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline, StyledEngineProvider } from '@mui/material';
+
+// routing
+import Routes from '@/dashboard/routes';
+
+// defaultTheme
+import themes from '@/dashboard/themes';
+
+// project imports
+import NavigationScroll from '@/dashboard/layout/NavigationScroll';
+
+import "@/dashboard/assets/scss/style.scss";
+
+// ==============================|| APP ||============================== //
+
+const App = () => {
+  const customization = useSelector((state) => state.customization);
+
+  return (
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={themes(customization)}>
+        <CssBaseline />
+        <NavigationScroll>
+          <Routes />
+        </NavigationScroll>
+      </ThemeProvider>
+    </StyledEngineProvider>
+  );
+};
+
+export default App;
