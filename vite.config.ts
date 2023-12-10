26b129afebd3dfbf410cfeeb0739b5bc26ca633a
sobonn <67411620+sobonn@users.noreply.github.com>
2023-12-10 23:38:40 +0900

    feat: berry-free-react-admin ダッシュボードの追加
    `/dashboard`以下にダッシュボードのテンプレートを貼り付けた

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 /// <reference types="vitest" />
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        alias: {
+            "@": resolve(dirname(fileURLToPath(import.meta.url)), "./src"),
+        },
+    },
     test: {
         include: ["**/*.{test,spec}.{ts,tsx}"],
     },
