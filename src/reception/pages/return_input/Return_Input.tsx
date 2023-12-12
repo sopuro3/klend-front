dfd0af93df3c3b3901a42a9d691f39f44ae4c6cf
chiepu3 <84263077+chiepu3@users.noreply.github.com>
2023-12-12 14:37:54 +0900

    わけわからん

--- src/reception/pages/return_input/Return_Input.tsx
+++ src/reception/pages/return_input/Return_Input.tsx
@@ -1,12 +1,18 @@
-import { Button, Card, CardContent, TextField, Typography } from "@mui/material";
+import {
+    Button,
+    Card,
+    CardContent,
+    TextField,
+    Typography,
+} from "@mui/material";
 
-export default function 返却時のナンバー入力画面(){
-   return (
-    <>
-        <h3 >受付番号の入力</h3>
-        <p>書類に記されている受付ナンバーを入力してください。</p>
+export default function 返却時のナンバー入力画面() {
+    return (
+        <>
+            <h3>受付番号の入力</h3>
+            <p>書類に記されている受付ナンバーを入力してください。</p>
 
-        <Card
+            <Card
                 sx={{
                     maxWidth: 300,
                     margin: "auto",
@@ -18,29 +24,34 @@ export default function 返却時のナンバー入力画面(){
                     <Typography variant="h5" component="div">
                         受付番号
                     </Typography>
-                        <App/>
+                    <App />
                 </CardContent>
             </Card>
-    </>
-   ) 
+        </>
+    );
 }
 
-
-import { useForm } from 'react-hook-form';
+import {  useForm } from "react-hook-form";
 
 function App() {
-  const { register, handleSubmit } = useForm();
+    const { register, handleSubmit } = useForm();
 
-  const onSubmit = (data: any) => console.log(data);
+    const onSubmit = (data: string) => console.log(data);
 
-  return (
-    <div className="App">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        
-        <TextField id="outlined-basic" label="4桁の数字" type="number" variant="outlined" {...register('number')} />
-        <Button variant="text" type="submit">決定</Button>
-      </form>
-    </div>
-  );
+    return (
+        <div className="App">
+            <form onSubmit={handleSubmit(onSubmit as any)}>
+                <TextField
+                    id="outlined-basic"
+                    label="4桁の数字"
+                    type="number"
+                    variant="outlined"
+                    {...register("number")}
+                />
+                <Button variant="text" type="submit">
+                    決定
+                </Button>
+            </form>
+        </div>
+    );
 }
-
