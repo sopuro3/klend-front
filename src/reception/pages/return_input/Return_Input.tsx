e847189ab0c129e2066daa4bfb56c4d82169fb8f
chiepu3 <84263077+chiepu3@users.noreply.github.com>
2023-12-12 13:00:46 +0900

    feat:簡単に画面を実装。

---
+++ src/reception/pages/return_input/Return_Input.tsx
@@ -0,0 +1,46 @@
+import { Button, Card, CardContent, TextField, Typography } from "@mui/material";
+
+export default function 返却時のナンバー入力画面(){
+   return (
+    <>
+        <h3 >受付番号の入力</h3>
+        <p>書類に記されている受付ナンバーを入力してください。</p>
+
+        <Card
+                sx={{
+                    maxWidth: 300,
+                    margin: "auto",
+                    padding: "10px",
+                    background: "#f8f8f8",
+                }}
+            >
+                <CardContent>
+                    <Typography variant="h5" component="div">
+                        受付番号
+                    </Typography>
+                        <App/>
+                </CardContent>
+            </Card>
+    </>
+   ) 
+}
+
+
+import { useForm } from 'react-hook-form';
+
+function App() {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data: any) => console.log(data);
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        
+        <TextField id="outlined-basic" label="4桁の数字" type="number" variant="outlined" {...register('number')} />
+        <Button variant="text" type="submit">決定</Button>
+      </form>
+    </div>
+  );
+}
+
