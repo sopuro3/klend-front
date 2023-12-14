199b76ce116ff56581f65536febb872c49403fe4
chiepu3 <84263077+chiepu3@users.noreply.github.com>
2023-12-14 10:56:37 +0900

    feat: パスワード入力画面を追加で実装

--- src/reception/pages/return_input/Return_Input.tsx
+++ src/reception/pages/return_input/Return_Input.tsx
@@ -1,20 +1,29 @@
+import { VisibilityOff, Visibility } from "@mui/icons-material";
 import {
     Button,
     Card,
     CardContent,
+    FormControl,
+    IconButton,
+    InputAdornment,
+    InputLabel,
+    OutlinedInput,
     TextField,
     Typography,
 } from "@mui/material";
+import { useState } from "react";
 
 export default function 返却時のナンバー入力画面() {
     return (
         <>
             <h3>受付番号の入力</h3>
-            <p>書類に記されている受付ナンバーを入力してください。</p>
+            <p>
+                書類に記されている受付ナンバーとパスワードを入力してください。
+            </p>
 
             <Card
                 sx={{
-                    maxWidth: 300,
+                    maxWidth: 400,
                     margin: "auto",
                     padding: "10px",
                     background: "#f8f8f8",
@@ -35,27 +44,73 @@ import { useForm } from "react-hook-form";
 
 type FormValues = {
     number: number;
+    password: string;
 };
 
 function App() {
     const { register, handleSubmit } = useForm<FormValues>();
 
     const onSubmit = (data: FormValues) => console.log(data);
+    const [showPassword, setShowPassword] = useState(false);
 
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (
+        event: React.MouseEvent<HTMLButtonElement>,
+    ) => {
+        event.preventDefault();
+    };
     return (
         <div className="App">
             <form onSubmit={handleSubmit(onSubmit)}>
-                <div style={{ display: "flex" }}>
+                <div>
                     <TextField
-                        id="outlined-basic"
-                        label="4桁の数字"
+                        label="4桁の受付No."
                         type="number"
                         variant="outlined"
                         {...register("number")}
+                        sx={{ marginBottom: "10px", width: "100%" }}
                     />
-                    <Button variant="contained" type="submit">
-                        決定
-                    </Button>
+
+                    <FormControl
+                        sx={{ width: "100%", marginBottom: "10px" }}
+                        variant="outlined"
+                    >
+                        <InputLabel htmlFor="outlined-adornment-password">
+                            パスワード
+                        </InputLabel>
+                        <OutlinedInput
+                            id="outlined-adornment-password"
+                            type={showPassword ? "text" : "password"}
+                            {...register("password")}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                        edge="end"
+                                    >
+                                        {showPassword ? (
+                                            <VisibilityOff />
+                                        ) : (
+                                            <Visibility />
+                                        )}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                            label="パスワード"
+                        />
+                    </FormControl>
+                    <div style={{ display: "flex", marginLeft: "auto" }}>
+                        <Button
+                            variant="contained"
+                            type="submit"
+                            sx={{ marginLeft: "auto" }}
+                        >
+                            決定
+                        </Button>
+                    </div>
                 </div>
             </form>
         </div>
