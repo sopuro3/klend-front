495de3d945ba25240e07bcb211c2302412445d75
sobonn <67411620+sobonn@users.noreply.github.com>
2023-12-15 21:37:47 +0900

    chore: ログインページを日本語化

--- src/dashboard/ui-component/cards/AuthFooter.jsx
+++ src/dashboard/ui-component/cards/AuthFooter.jsx
@@ -5,11 +5,8 @@ import { Link, Typography, Stack } from '@mui/material';
 
 const AuthFooter = () => (
   <Stack direction="row" justifyContent="space-between">
-    <Typography variant="subtitle2" component={Link} href="https://berrydashboard.io" target="_blank" underline="hover">
-      berrydashboard.io
-    </Typography>
-    <Typography variant="subtitle2" component={Link} href="https://codedthemes.com" target="_blank" underline="hover">
-      &copy; codedthemes.com
+    <Typography variant="subtitle2" component={Link} target="_blank" underline="hover">
+      K-Lend
     </Typography>
   </Stack>
 );
