499426d6575cef298fe4e920fe21d93f7af6548b
chiepu3 <84263077+chiepu3@users.noreply.github.com>
2023-12-15 14:15:52 +0900

    feat:情報を吸い出せるようになった

--- src/pages/needs-form/NeedsForm.tsx
+++ src/pages/needs-form/NeedsForm.tsx
@@ -8,6 +8,7 @@ export default function NeedsForm() {
 }
 
 import "./needsform.css";
+import { useForm } from "react-hook-form";
 
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -18,68 +19,96 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import TextField from "@mui/material/TextField";
 import { Button } from "@mui/material";
-function createData(
-    name: string,
-    type:
-        | typeof String
-        | typeof Number
-        | typeof Boolean
-        | typeof Date
-        | typeof Array
-        | typeof Object,
-) {
-    return { name, type };
+
+type rowData = {
+    name: string;
+    FormName: "name" | "address";
+};
+function createData(name: string, FormName: "name" | "address"): rowData {
+    return { name, FormName };
 }
 
 const rows = [
-    createData("被災宅の名前(仮)", String),
-    createData("住所", String),
+    createData("被災宅の名前(仮)", "name"),
+    createData("住所", "address"),
 ];
 
+type FormValues = {
+    name: string;
+    address: string;
+};
+
 export function BasicTable() {
+    const { register, handleSubmit } = useForm<FormValues>();
+    const onSubmit = (data: FormValues) => {
+        console.log(data);
+    };
+
     return (
         <>
-            <TableContainer component={Paper}>
-                <Table className="single-row-table" aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>項目</TableCell>
-                            <TableCell>入力欄</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.map((row) => (
-                            <TableRow
-                                key={row.name}
-                                sx={{
-                                    "&:last-child td, &:last-child th": {
-                                        border: 0,
-                                    },
-                                }}
-                            >
-                                <TableCell component="th" scope="row">
-                                    {row.name}
-                                </TableCell>
-                                <TableCell>
-                                    <TextField
-                                        required
-                                        sx={{ width: "100%" }}
-                                        id="outlined-basic"
-                                        label="入力必須"
-                                        variant="outlined"
-                                    />
-                                </TableCell>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <TableContainer component={Paper}>
+                    <Table
+                        className="single-row-table"
+                        aria-label="simple table"
+                    >
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>項目</TableCell>
+                                <TableCell>入力欄</TableCell>
                             </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <br></br>
-            <div style={{ display: "flex" }}>
-                <Button variant="contained" sx={{ marginLeft: "auto" }}>
-                    送信
-                </Button>
-            </div>
+                        </TableHead>
+                        <TableBody>
+                            <RowItem row={rows[0]} />
+                            <RowItem row={rows[1]} />
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <br></br>
+                <div style={{ display: "flex" }}>
+                    <Button
+                        variant="contained"
+                        sx={{ marginLeft: "auto" }}
+                        type="submit"
+                    >
+                        送信
+                    </Button>
+                </div>
+            </form>
         </>
     );
+
+    function RowItem(props: RowItemProps) {
+        const { row } = props;
+        return (
+            <>
+                <TableRow
+                    key={row.name}
+                    sx={{
+                        "&:last-child td, &:last-child th": {
+                            border: 0,
+                        },
+                    }}
+                >
+                    <TableCell component="th" scope="row">
+                        {row.name}
+                    </TableCell>
+                    <TableCell>
+                        <TextField
+                            required
+                            sx={{ width: "100%" }}
+                            id="outlined-basic"
+                            label="入力必須"
+                            variant="outlined"
+                            {...register(row.FormName, { required: true })}
+                        />
+                    </TableCell>
+                </TableRow>
+            </>
+        );
+    }
 }
+
+type RowItemProps = {
+    row: rowData;
+};
