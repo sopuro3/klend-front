ec2596aaac1317c96109b97b8f761aa7b206e26f
chiepu3c <84263077+chiepu3@users.noreply.github.com>
2023-12-22 21:46:01 +0900

    たぶんfmt

--- src/dashboard/views/delend_select/index.tsx
+++ src/dashboard/views/delend_select/index.tsx
@@ -1,4 +1,3 @@
-import { LendForm } from "@/API/API_interface";
 import { EquipmentSuper } from "@/API/Data_manage";
 import { WithoutWrapper_Case } from "@/components/Case_Detail/Case_Page";
 import { SelectableStockTable } from "@/components/Stock_Table/StockTable";
@@ -9,49 +8,39 @@ import { Button, useTheme } from "@mui/material";
 import { useState } from "react";
 // import { useForm } from "react-hook-form";
 
-
-
-
 export default function Delend_select() {
-
     const theme = useTheme();
-
-
 
     const [value, setValue] = useState<EquipmentSuper>({
         equipmentsRequired: [],
         equipmentswithQuantity: [],
     });
 
-    
-
     const onClick = () => {
         console.log(value);
-    }
-
+    };
 
     return (
         <>
             <PageTitle title={"貸出数の確定"} />
             <MainCard_ts>
-                    <WithoutWrapper_Case />
+                <WithoutWrapper_Case />
 
-                    <SelectableStockTable setVal={setValue} />
+                <SelectableStockTable setVal={setValue} />
 
-                    <div style={{ display: "flex" }}>
-                                    <Button 
-                                        onClick={onClick}
-                                        variant="contained"
-                                        sx={{
-                                            marginLeft: "auto",
-                                            background:
-                                                theme.palette.primary.main,
-                                        }}
-                                        type="submit"
-                                    >
-                                        確認
-                                    </Button>
-                                </div>
+                <div style={{ display: "flex" }}>
+                    <Button
+                        onClick={onClick}
+                        variant="contained"
+                        sx={{
+                            marginLeft: "auto",
+                            background: theme.palette.primary.main,
+                        }}
+                        type="submit"
+                    >
+                        確認
+                    </Button>
+                </div>
             </MainCard_ts>
         </>
     );
