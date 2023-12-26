2313689c94fdf0cfdba70706548a7dde3df9a460
chiepu3c <84263077+chiepu3@users.noreply.github.com>
2023-12-26 14:50:15 +0900

    fix: cls to typ

--- src/components/shikizai-table/interface.ts
+++ src/components/shikizai-table/interface.ts
@@ -1,15 +1,7 @@
-export class columnItem {
+export type columnItem = {
     name: string;
     description: string;
     type: string;
     stock: number;
     requested: number;
-
-    constructor({ name = "", description = "", type = "", stock = 0 }) {
-        this.name = name;
-        this.requested = 0;
-        this.description = description;
-        this.type = type;
-        this.stock = stock;
-    }
-}
+};
