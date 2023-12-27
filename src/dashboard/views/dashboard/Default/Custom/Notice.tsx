d67b25642c0a12e72c259c3444a9045c01aee339
chiepu3c <84263077+chiepu3@users.noreply.github.com>
2023-12-27 14:56:45 +0900

    fmt

--- src/dashboard/views/dashboard/Default/Custom/Notice.tsx
+++ src/dashboard/views/dashboard/Default/Custom/Notice.tsx
@@ -5,7 +5,6 @@ import { Card, Paper } from "@mui/material";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { Suspense } from "react";
 import "./Notice.css";
-import { height } from "@mui/system";
 export default function Notice() {
     return (
         <Suspense fallback={<PageLoader />}>
