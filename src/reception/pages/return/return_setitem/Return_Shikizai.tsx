0f81301082e03ca5e052067ad24eb6d519ab598d
chiepu3c <84263077+chiepu3@users.noreply.github.com>
2023-12-29 16:29:27 +0900

    isConfirm準備

--- src/reception/pages/return/return_setitem/Return_Shikizai.tsx
+++ src/reception/pages/return/return_setitem/Return_Shikizai.tsx
@@ -1,9 +1,12 @@
 import { EquipmentSuper } from "@/API/Data_manage";
 import { SelectableStockTable } from "@/components/Stock_Table/StockTable";
-import { Button } from "@mui/material";
+import { Button, Link } from "@mui/material";
 import { useState } from "react";
+import { Link as RouterLink } from "react-router-dom";
 
 export default function 返却フォームの資機材入力画面() {
+    const [isConfirm, setIsConfirm] = useState(false);
+
     const [value, setValue] = useState<EquipmentSuper>({
         equipmentsRequired: [],
         equipmentswithQuantity: [],
@@ -11,21 +14,34 @@ export default function 返却フォームの資機材入力画面() {
     console.log(value);
     return (
         <>
-            <h3>返却した資機材の数量確認</h3>
-            <p>
-                破損・紛失した資機材は除き、返却した資機材の数量を確認してください。
-            </p>
-            <SelectableStockTable setVal={setValue}></SelectableStockTable>
-            <br/>
-            <div style={{ display: "flex" }}>
-                <Button
-                    variant="contained"
-                    href="/reception/return/done"
-                    sx={{ marginLeft: "auto" }}
-                >
-                    送信
-                </Button>
-            </div>
+            {!isConfirm ? (
+                <>
+                    <h3>返却した資機材の数量確認</h3>
+                    <p>
+                        破損・紛失した資機材は除き、返却した資機材の数量を確認してください。
+                    </p>
+                    <SelectableStockTable
+                        setVal={setValue}
+                    ></SelectableStockTable>
+                    <br />
+                    <div style={{ display: "flex" }}>
+                        <Button
+                            variant="contained"
+                            sx={{ marginLeft: "auto" }}
+                            onClick={() => {
+                                console.log(value);
+                            }}
+                        >
+                            送信
+                        </Button>
+                    </div>
+                    <Link component={RouterLink} to="/reception/return/done">
+                        完了画面へ
+                    </Link>
+                </>
+            ) : (
+                <h3>送信完了</h3>
+            )}
         </>
     );
 }
