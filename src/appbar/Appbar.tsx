b2f9e3fc052078440fc5b48350eb6c469e33bf30
chiepu3c <84263077+chiepu3@users.noreply.github.com>
2024-01-17 21:49:51 +0900

    fix:iOSでNoto sansが使えない問題を解消できるかのテスト

--- src/appbar/Appbar.tsx
+++ src/appbar/Appbar.tsx
@@ -112,10 +112,11 @@ function ResponsiveAppBar() {
                             mr: 2,
                             display: { xs: "flex", md: "none" },
                             flexGrow: 1,
-                            fontFamily: "monospace",
+                            fontFamily: "Noto Sans JP",
                             fontWeight: 700,
                             letterSpacing: ".3rem",
                             color: "inherit",
+
                             textDecoration: "none",
                         }}
                     >
