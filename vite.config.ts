a0331996cc7a59527b4320cf83bd3df2b09e2b52
Yuki Yuigishi <me@yuigishi.dev>
2024-01-22 15:10:04 +0900

    chore:add suplitVendorChunkPlugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 /// <reference types="vitest" />
 import { fileURLToPath } from "url";
 import { dirname, resolve } from "path";
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), splitVendorChunkPlugin()],
     resolve: {
         alias: {
             "@": resolve(dirname(fileURLToPath(import.meta.url)), "./src"),
